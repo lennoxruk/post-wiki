da4601a5a6f0e27d8cbf5f2267817de8a6fe39e7
lennox <lennox@noreply.com>
2023-10-20 15:46:32 +0100

    fix: add access token for repository clone

--- post-wiki.sh
+++ post-wiki.sh
@@ -8,7 +8,10 @@ trap 'rm -rf "$tmp_dir"' SIGINT SIGTERM ERR EXIT
 # https://weblog.west-wind.com/posts/2023/Jan/05/Fix-that-damn-Git-Unsafe-Repository
 git config --global --add safe.directory "$tmp_dir"
 
-git clone "$INPUT_GITEA_SERVER_URL/$INPUT_REPOSITORY.wiki.git" "$tmp_dir"
+PROTO="`echo $INPUT_GITEA_SERVER_URL | grep '://' | sed -e's,^\(.*://\).*,\1,g'`"
+URL=`echo $INPUT_GITEA_SERVER_URL | sed -e s,$proto,,g`
+
+git clone "$PROTO$INPUT_TOKEN$URL/$INPUT_REPOSITORY.wiki.git" "$tmp_dir"
 
 # Hidden files (like .myfile.txt, .git/, or .gitignore) are NOT copied.
 rm -rf "${tmp_dir:?}"/*
